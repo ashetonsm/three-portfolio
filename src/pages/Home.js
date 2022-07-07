7f1f835402275408439c5b58f8d1e5e21e9ee132
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2022-07-07 16:56:55 -0500

    Box components are rendered via map

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,28 @@
 import { Canvas } from "@react-three/fiber"
+import { useState } from "react";
 import { Box } from "../components/models/Box"
 import { ButtonPlane } from "../components/UI/ButtonPlane";
 
-export const Home = ({props}) => {
+export const Home = ({ props }) => {
+
+    const [allItems, setAllItems] = useState([
+        {name: 'box1', position:[-1.2, 0, 0]},  {name: 'box2', position:[1.2, 0, 0]}
+    ]);
+
+    const addItemHandler = (newItem) => {
+        setAllItems((currentItems) => [
+            ...currentItems, {
+                obj: newItem, id: Math.random().toString()
+            }
+        ]);
+    }
 
     const goLeft = () => {
         console.log("Left")
+    }
+
+    const goRight = () => {
+        console.log("Right")
     }
 
     return (
@@ -13,10 +30,13 @@ export const Home = ({props}) => {
             <ambientLight intensity={0.5} />
             <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
             <pointLight position={[-10, -10, -10]} />
-            <Box position={[-1.2, 0, 0]} />
-            <Box position={[1.2, 0, 0]} />
-            <ButtonPlane position={[-10, 0, 0]} />
-            <ButtonPlane position={[10, 0, 0]} />
+
+            {allItems.map((item) => (
+                <Box key={item.name} position={item.position} />
+            ))}
+
+            <ButtonPlane position={[-10, 0, 0]} handleClick={goLeft} />
+            <ButtonPlane position={[10, 0, 0]} handleClick={goRight} />
         </Canvas>
     )
 }
