c64cadecda2b4c4a9b22d864183909fb5c1da346
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2022-07-07 14:28:46 -0500

    Placeholders for on-screen arrow controls

--- src/components/models/Box.js
+++ src/components/models/Box.js
@@ -4,6 +4,7 @@ import { useFrame } from '@react-three/fiber'
 export const Box = (props) => {
     const ref = useRef()
     const [hovered, hover] = useState(false)
+    const [active, isActive] = useState(false)
     const [clicked, click] = useState(false)
 
     // Subscribe this component to the render-loop, rotate the mesh every frame
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,19 @@
 import { Canvas } from "@react-three/fiber"
 import { Box } from "../components/models/Box"
+import { FiArrowLeft, FiArrowRight } from 'react-icons/fi';
+import { Button } from '@chakra-ui/react'
 
-export const Home = (props) => {
+export const Home = ({props}) => {
+
+    const goLeft = () => {
+        console.log("Left")
+    }
+
+
     return (
+        <>
+        <Button onClick={goLeft}> <FiArrowLeft /> </Button>
+        <Button> <FiArrowRight /> </Button>
         <Canvas>
             <ambientLight intensity={0.5} />
             <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
@@ -10,5 +21,6 @@ export const Home = (props) => {
             <Box position={[-1.2, 0, 0]} />
             <Box position={[1.2, 0, 0]} />
         </Canvas>
+        </>
     )
 }
