f25afdde7e0fd285ccb4788c00517cc6230adf58
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2022-07-07 14:42:48 -0500

    Replaced buttons with ButtonPlanes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,6 @@
 import { Canvas } from "@react-three/fiber"
 import { Box } from "../components/models/Box"
-import { FiArrowLeft, FiArrowRight } from 'react-icons/fi';
-import { Button } from '@chakra-ui/react'
+import { ButtonPlane } from "../components/UI/ButtonPlane";
 
 export const Home = ({props}) => {
 
@@ -9,18 +8,15 @@ export const Home = ({props}) => {
         console.log("Left")
     }
 
-
     return (
-        <>
-        <Button onClick={goLeft}> <FiArrowLeft /> </Button>
-        <Button> <FiArrowRight /> </Button>
         <Canvas>
             <ambientLight intensity={0.5} />
             <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
             <pointLight position={[-10, -10, -10]} />
             <Box position={[-1.2, 0, 0]} />
             <Box position={[1.2, 0, 0]} />
+            <ButtonPlane position={[-10, 0, 0]} />
+            <ButtonPlane position={[10, 0, 0]} />
         </Canvas>
-        </>
     )
 }
