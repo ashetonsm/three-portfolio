90413bd4b4a480a6905438a2f2f137fca04ec263
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2022-07-07 14:28:25 -0500

    Experimenting with router

--- src/App.js
+++ src/App.js
@@ -2,17 +2,29 @@ import './App.css';
 import { Home } from './pages/Home';
 import { ChakraProvider } from '@chakra-ui/react'
 import SidebarWithHeader from './components/UI/Sidebar.tsx';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 function App() {
+
   return (
     <>
-    <ChakraProvider>
-      
-      <SidebarWithHeader>
-        <Home/>
-      </SidebarWithHeader>
-      
-    </ChakraProvider>
+      <BrowserRouter>
+        <ChakraProvider>
+          <SidebarWithHeader>
+            <Home/>
+          </SidebarWithHeader>
+
+          <Routes>
+            <Route path="/" element={<SidebarWithHeader></SidebarWithHeader>} />
+            <Route path="/test" element={<div>Hello world</div>} />
+          </Routes>
+
+        </ChakraProvider>
+      </BrowserRouter>
     </>
   );
 }
