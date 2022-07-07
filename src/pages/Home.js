4ce28c83c4abb89b0f4703ff6b12613b6755b798
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2022-07-07 11:05:37 -0500

    Will serve as the main page

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import { Canvas } from "@react-three/fiber"
+import { Box } from "../components/models/Box"
+
+export const Home = (props) => {
+    return (
+        <Canvas>
+            <ambientLight intensity={0.5} />
+            <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+            <pointLight position={[-10, -10, -10]} />
+            <Box position={[-1.2, 0, 0]} />
+            <Box position={[1.2, 0, 0]} />
+        </Canvas>
+    )
+}
