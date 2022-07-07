a71bc8c7618ce5762641ed2d79d7201b885e53de
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2022-07-07 11:06:07 -0500

    A simple box from the react-three-fiber example.

---
+++ src/components/models/Box.js
@@ -0,0 +1,24 @@
+import { useState, useRef } from "react"
+import { useFrame } from '@react-three/fiber'
+
+export const Box = (props) => {
+    const ref = useRef()
+    const [hovered, hover] = useState(false)
+    const [clicked, click] = useState(false)
+
+    // Subscribe this component to the render-loop, rotate the mesh every frame
+    useFrame((state, delta) => (ref.current.rotation.x += 0.01))
+
+    return (
+        <mesh
+            {...props}
+            ref={ref}
+            scale={clicked ? 1.5 : 1}
+            onPointerOver={(event) => hover(true)}
+            onPointerOut={(event) => hover(false)}>
+            <boxGeometry args={[1, 1, 1]} />
+            <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+        </mesh>
+    )
+
+}
