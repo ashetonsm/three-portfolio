0f5972e0946d7c65de6ea7aec73945d21635f581
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2022-07-08 16:04:05 -0500

    Sets position, calls the onActive function onClick

--- src/components/models/Box.js
+++ src/components/models/Box.js
@@ -1,34 +1,30 @@
-import { useEffect, useState, useRef } from "react"
+import { useState, useRef } from "react"
 import { useFrame } from '@react-three/fiber'
 
 export const Box = (props) => {
+
     const ref = useRef()
     const [hovered, hover] = useState(false)
-    const [clicked, click] = useState(false)
-
+    const [active, setActive] = useState(false)
+    
     // Subscribe this component to the render-loop, rotate the mesh every frame
     useFrame((state, delta) => (ref.current.rotation.x += 0.01))
 
-    // useEffect(() => {
-    //     if (props.name === props.activeItem.name) {
-    //         console.log("Active matches this box's name")
-    //     }
-    // });
-
-    const report = () => {
-        console.log("This item's name is: " + "" + "\nThe active item's name is: " + "")
+    const handleClick = (obj) => {
+        setActive(true)
+        obj.position.set(0, 0, 0)
+        props.onActive(obj)
     }
 
     return (
         <mesh
             {...props}
             ref={ref}
-            scale={clicked ? 1.5 : 1}
-            onClick={(event) => report()}
+            onClick={(event) => handleClick(event.object)}
             onPointerOver={(event) => hover(true)}
             onPointerOut={(event) => hover(false)}>
             <boxGeometry args={[1, 1, 1]} />
-            <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+            <meshStandardMaterial color={hovered ? '#474747' : props.color} />
         </mesh>
     )
 
