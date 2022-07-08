0a7c778d9682ab505d5b139aa0fa7f131faba371
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2022-07-08 16:00:34 -0500

    Color, name, onActive props.
    Sets the clicked item as active and makes all other meshes inactive.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,44 +7,45 @@ export const Home = ({ props }) => {
 
 
     const [allItems, setAllItems] = useState([
-        { name: 'box1', position: [-1.2, 0, 0] },
-        { name: 'box2', position: [1.2, 0, 0] }
+        { name: 'box0', position: [0, 0, 0], color: '#ff82d9' },
+        { name: 'box1', position: [2, 0, 0], color: '#00ff0d' },
+        { name: 'box2', position: [4, 0, 0], color: '#0051ff' },
+        { name: 'box3', position: [6, 0, 0], color: '#ff4343' },
     ]);
 
-    // useEffect(() => {
-    //   console.log("UseEffect")
-    // }, [])
+    const [activeItem, setActiveItem] = useState({})
 
+    const makeActive = (obj) => {
+        setActiveItem(obj)
+        // console.log(obj.parent.children)
+        // console.log(obj)
 
-    const [activeItem, setActiveItem] = useState(allItems.at(0))
-    let currentItem = 0;
-    let totalItems = allItems.length;
-
-    const goLeft = () => {
-
+        // Find the meshes that need to be marked inactive
+        const inactiveMeshes = obj.parent.children.filter((child) => child.type === 'Mesh' && child.uuid !== obj.uuid);
+        removeActive(inactiveMeshes)
     }
 
-    const goRight = () => {
-
+    const removeActive = (objList) => {
+        let increment = 2;
+        objList.forEach(mesh => {
+            mesh.position.set(increment, 0, 0)
+            increment = increment + 2
+        });
     }
-
 
     return (
         <>
-            <div>
-                {activeItem.name}
-            </div>
-            <Canvas>
+        <div>
+            {activeItem.name !== undefined ? activeItem.name : "No active item"}
+        </div>
+            <Canvas tabIndex={0} >
                 <ambientLight intensity={0.5} />
                 <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
                 <pointLight position={[-10, -10, -10]} />
 
                 {allItems.map((item) => (
-                    <Box key={item.name} position={item.position} activeItem={allItems.at(currentItem)} />
+                    <Box key={item.name} position={item.position} color={item.color} onActive={makeActive} name={item.name} />
                 ))}
-
-                <ButtonPlane position={[-10, 0, 0]} handleClick={goLeft} />
-                <ButtonPlane position={[10, 0, 0]} handleClick={goRight} />
             </Canvas>
         </>
     )
