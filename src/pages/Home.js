998fa51be19d7266339955fd64067230adf204df
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2022-07-11 12:08:29 -0500

    Restructured to get around models being in groups

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,42 +1,51 @@
-import { Canvas } from "@react-three/fiber"
+import { Canvas, extend } from "@react-three/fiber"
 import { useState } from "react";
-import { Box } from "../components/models/Box"
+import Phone from '../components/models/Phone'
+import Keyboard from '../components/models/Keyboard'
+import Pen from '../components/models/Pen'
+
+extend({ Phone, Keyboard, Pen})
 
 export const Home = ({ props }) => {
 
-
     const [allItems, setAllItems] = useState([
-        { name: 'box0', position: [0, 0, 0], color: '#ff82d9' },
-        { name: 'box1', position: [2, 0, 0], color: '#00ff0d' },
-        { name: 'box2', position: [4, 0, 0], color: '#0051ff' },
-        { name: 'box3', position: [6, 0, 0], color: '#ff4343' },
+        { name: 'Phone', position: [0, 0, 0], color: '#ff82d9' },
+        { name: 'Keyboard', position: [2, 0, 0], color: '#00ff0d' },
+        { name: 'Pen', position: [4, 0, 0], color: '#0051ff' },
     ]);
 
     const [activeItem, setActiveItem] = useState({})
 
     const makeActive = (obj) => {
-        setActiveItem(obj)
-        // console.log(obj.parent.children)
-        // console.log(obj)
+        // The Group that this Object is nested in
+        setActiveItem(obj.parent)
+
+        // The parent of this group (the canvas)
+        //console.log(obj.parent.parent.children)
+
+        //console.log(obj.parent)
 
         // Find the meshes that need to be marked inactive
-        const inactiveMeshes = obj.parent.children.filter((child) => child.type === 'Mesh' && child.uuid !== obj.uuid);
+        // Compare the group UUIDs
+        const inactiveMeshes = obj.parent.parent.children.filter((child) => child.type === 'Group' && child.uuid !== obj.parent.uuid);
         removeActive(inactiveMeshes)
     }
 
     const removeActive = (objList) => {
         let increment = 2;
         objList.forEach(mesh => {
+            //console.log(mesh.children[0])
+            mesh.children[0].makeInactive()
+            //mesh.children[0].position.set(increment, 0, 0)
             mesh.position.set(increment, 0, 0)
             increment = increment + 2
-            mesh.makeInactive()
         });
     }
 
     return (
         <>
         <div>
-            {activeItem.name !== undefined ? activeItem.name : "No active item"}
+            {activeItem.name !== undefined ? activeItem.name : "Choose Your Weapon"}
         </div>
             <Canvas tabIndex={0} >
                 <ambientLight intensity={0.5} />
@@ -44,8 +53,19 @@ export const Home = ({ props }) => {
                 <pointLight position={[-10, -10, -10]} />
 
                 {allItems.map((item) => (
-                    <Box key={item.name} position={item.position} color={item.color} onActive={makeActive} name={item.name} />
+                    item.name === "Keyboard" ?
+                        <Keyboard key={item.name} position={item.position} color={item.color} onActive={makeActive} name={item.name} /> :
+
+                    item.name === "Pen" ?
+                        <Pen key={item.name} position={item.position} color={item.color} onActive={makeActive} name={item.name} /> :
+
+                    item.name === "Phone" ?
+                        <Phone key={item.name} position={item.position} color={item.color} onActive={makeActive} name={item.name} /> :
+
+                        null
+                    
                 ))}
+                
             </Canvas>
         </>
     )
