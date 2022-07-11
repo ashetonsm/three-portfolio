7e97a037e527f81b3ea0b8d108d5fdf752cd79a0
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2022-07-11 17:44:20 -0500

    Added new routes for 3D, 2D, and contact page.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css'
 import { Home } from './pages/Home'
+import { Contact } from './pages/Contact'
 import { ChakraProvider } from '@chakra-ui/react'
 import SidebarWithHeader from './components/UI/Sidebar.tsx'
 import {
@@ -7,6 +8,8 @@ import {
   Routes,
   Route,
 } from "react-router-dom";
+import { Gallery2D } from './pages/Gallery2D'
+import { Gallery3D } from './pages/Gallery3D'
 
 function App() {
 
@@ -14,12 +17,33 @@ function App() {
     <>
       <BrowserRouter>
         <ChakraProvider>
-          <SidebarWithHeader>
-            <Home/>
-          </SidebarWithHeader>
+
 
           <Routes>
-            <Route path="/test" element={<div>Hell, world!</div>} />
+            <Route path="/"
+              element={
+                <SidebarWithHeader>
+                  <Home />
+                </SidebarWithHeader>
+              } />
+            <Route path="/gallery3D"
+              element={
+                <SidebarWithHeader>
+                  <Gallery3D />
+                </SidebarWithHeader>
+              } />
+            <Route path="/gallery2D"
+              element={
+                <SidebarWithHeader>
+                  <Gallery2D />
+                </SidebarWithHeader>
+              } />
+            <Route path="/contact"
+              element={
+                <SidebarWithHeader>
+                  <Contact />
+                </SidebarWithHeader>
+              } />
           </Routes>
 
         </ChakraProvider>
