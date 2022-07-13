1be2791306c2a832fa1e684d35b707998a0cd0ae
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2022-07-13 16:05:19 -0500

    Set up a basic text displaying HTML component

--- src/components/models/Text3D.js
+++ src/components/models/Text3D.js
@@ -1,6 +1,26 @@
-export const Text3D = ({children}) => {
+import React, { useState, useRef } from 'react'
+import { Canvas, useFrame } from '@react-three/fiber'
+import { OrbitControls, Html } from '@react-three/drei'
 
+export const Text3D = ({children}) => {
+    const [size, set] = useState(0.5)
+    const [hidden, setVisible] = useState(false)
     return (
-        <div>{children}</div>
-    )
+        <mesh scale={size * 2}>
+        <meshStandardMaterial />
+        <Html
+          style={{
+            transition: 'all 0.2s',
+            opacity: hidden ? 0 : 1,
+            transform: `scale(${hidden ? 0.5 : 1})`
+          }}
+          distanceFactor={1.5}
+          position={[0, 1, 0]}
+          transform
+          occlude
+          onOcclude={setVisible}>
+          <span>{children}</span>
+        </Html>
+      </mesh>    
+      )
 }
