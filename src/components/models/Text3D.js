c2c3d4d103612a1a0f8ce93660e8ace3ebafd1ba
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2022-07-14 17:53:36 -0500

    Text rotates to face camera

--- src/components/models/Text3D.js
+++ src/components/models/Text3D.js
@@ -1,26 +1,28 @@
 import React, { useState, useRef } from 'react'
-import { Canvas, useFrame } from '@react-three/fiber'
-import { OrbitControls, Html } from '@react-three/drei'
+import { Html } from '@react-three/drei'
+import { useFrame } from '@react-three/fiber'
 
-export const Text3D = ({children}) => {
-    const [size, set] = useState(0.5)
-    const [hidden, setVisible] = useState(false)
-    return (
-        <mesh scale={size * 2}>
-        <meshStandardMaterial />
-        <Html
-          style={{
-            transition: 'all 0.2s',
-            opacity: hidden ? 0 : 1,
-            transform: `scale(${hidden ? 0.5 : 1})`
-          }}
-          distanceFactor={1.5}
-          position={[0, 1, 0]}
-          transform
-          occlude
-          onOcclude={setVisible}>
-          <span>{children}</span>
-        </Html>
-      </mesh>    
-      )
+export const Text3D = ({ children, props }) => {
+  const [size, set] = useState(0.5)
+  const [hidden, setVisible] = useState(false)
+  const ref = useRef()
+  useFrame(({ camera }) => ref.current.quaternion.copy(camera.quaternion))
+
+  return (
+    <mesh ref={ref} scale={size * 2} >
+      <Html
+        style={{
+          transition: 'all 0.2s',
+          opacity: hidden ? 0 : 1,
+          transform: `scale(${hidden ? 0.5 : 1})`
+        }}
+        distanceFactor={5}
+        position={[0, 1, 0]}
+        transform
+        occlude
+        onOcclude={setVisible}>
+        <span>{children}</span>
+      </Html>
+    </mesh>
+  )
 }
