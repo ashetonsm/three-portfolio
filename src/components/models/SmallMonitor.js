55529b74bad9174ffc102600a4896ada7fe75d3f
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2022-07-19 18:58:02 -0500

    Corrected pivot center for GLTF model

--- src/components/models/SmallMonitor.js
+++ src/components/models/SmallMonitor.js
@@ -4,48 +4,23 @@ import { Select } from '@react-three/postprocessing'
 
 export default function Model({ ...props }) {
 
-    const [hovered, hover] = useState(null)
-    const [active, setActive] = useState(false)
     const group = useRef();
     const { nodes, materials } = useGLTF("models/SmallMonitor.gltf");
 
-    const handleClick = (obj) => {
-        if (!active === false) {
-            props.onInactive()
-        } else {
-            props.onActive(obj)
-        }
-        setActive(!active)
-    }
-
-    const hovering = (obj) => {
-        hover(true)
-        props.onHover(obj)
-    }
-
-    const notHovering = () => {
-        if (!active) {
-            hover(false)
-        }
-        props.onExitHover()
-    }
     return (
-        <Select enabled={hovered}>
+        <Select>
 
             <group
                 ref={group}
                 {...props}
-                dispose={null}
-                onPointerOver={(event) => hovering(event.object.parent)}
-                onPointerOut={(event) => notHovering()}
-                onClick={(event) => handleClick(event.object.parent)} >
+                dispose={null}>
 
                 <mesh
                     castShadow
                     receiveShadow
                     geometry={nodes.Small_Monitor_Screen.geometry}
                     material={materials["Small Monitor"]}
-                    position={[-0.03, 0, 0.06]}
+                    position={[-0.31, 0.51, -0.07]}
                 />
             </group>
         </Select>
