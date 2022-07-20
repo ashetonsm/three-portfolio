d42724705c47a3aaef034157e27833e533dbf256
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2022-07-20 10:58:48 -0500

    Adjusted to display an anchor link to an address

--- src/components/models/Text3D.js
+++ src/components/models/Text3D.js
@@ -1,5 +1,5 @@
 import React, { useState, useRef } from 'react'
-import { Html } from '@react-three/drei'
+import { Html, Text } from '@react-three/drei'
 import { useFrame } from '@react-three/fiber'
 
 export const Text3D = ({ children, props }) => {
@@ -10,18 +10,30 @@ export const Text3D = ({ children, props }) => {
 
   return (
     <mesh ref={ref} scale={size * 2} >
-      <Html
+      <Text
+        color='#000'
+        fontSize={0.2}
+        position={[0, 2, 0]}
+      >
+        {children[0]}
+      </Text>
+
+      <Html {...props}
         style={{
           transition: 'all 0.2s',
           opacity: hidden ? 0 : 1,
           transform: `scale(${hidden ? 0.5 : 1})`
         }}
         distanceFactor={5}
-        position={[0, 2, 0]}
+        position={[0, 1.8, 0]}
         transform
         occlude
         onOcclude={setVisible}>
-        <span>{children}</span>
+        <a
+          href={children[1]}
+          target="_blank"
+          rel="noreferrer noopener"
+        >{children[1]}</a>
       </Html>
     </mesh>
   )
