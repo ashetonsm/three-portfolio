55a1d0296522552d219253b9025213c942653913
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2022-07-22 14:33:32 -0500

    Texture logos added, clicked.current restructured

--- src/pages/DeskScene.js
+++ src/pages/DeskScene.js
@@ -1,8 +1,9 @@
 import * as THREE from 'three'
-import { Canvas, extend, useFrame } from "@react-three/fiber"
+import { Canvas, extend, useFrame, useLoader } from "@react-three/fiber"
 import { useRef, useState, useEffect } from "react"
-import { useCursor, OrbitControls } from '@react-three/drei'
+import { useCursor, OrbitControls, useTexture } from '@react-three/drei'
 import { Selection, EffectComposer, Outline } from '@react-three/postprocessing'
+import { TextureLoader } from "three/src/loaders/TextureLoader"
 import BigMonitor from '../components/models/BigMonitor'
 import SmallMonitor from '../components/models/SmallMonitor'
 import Keyboard from '../components/models/Keyboard'
@@ -10,9 +11,12 @@ import Tablet from '../components/models/Tablet'
 import Desk from '../components/models/Desk'
 import Computer from '../components/models/Computer'
 import Chair from '../components/models/Chair'
+import { TestBox } from '../components/models/TestBox'
+import { BigScreen } from '../components/models/BigScreen'
 import { Box } from '../components/models/Box'
 import { Text3D } from '../components/models/Text3D'
 import { useRoute, useLocation } from 'wouter'
+import getUuidByString from 'uuid-by-string'
 
 extend({ BigMonitor, Keyboard, Tablet })
 
@@ -20,6 +24,19 @@ export const DeskScene = ({ props }) => {
     const GOLDENRATIO = 1.61803398875
     const [activeItem, setActiveItem] = useState()
     const [activeURL, setActiveURL] = useState()
+    const [showPanelR, setShowPanelR] = useState(false)
+    const [showPanelL, setShowPanelL] = useState(false)
+
+    const [artstationTex, githubTex, itchTex] = useLoader(TextureLoader,
+        [
+            '/three-portfolio/textures/artstation-logo.png',
+            '/three-portfolio/textures/github-logo.png',
+            '/three-portfolio/textures/itchio-logo.png',
+        ])
+
+
+    const [bigMonTex, setBigMonTex] = useState(artstationTex)
+    const [smallMonTex, setSmallMonTex] = useState(artstationTex)
 
     const interatives = [
         { model: BigMonitor, modelName: "BigMonitor", linkText: "Github", url: "https://github.com/ashetonsm" },
@@ -35,18 +52,45 @@ export const DeskScene = ({ props }) => {
         const [, setLocation] = useLocation()
         useEffect(() => {
             clicked.current = ref.current.getObjectByName(params?.id)
-            // console.log(clicked.current)
             if (clicked.current) {
-                clicked.current.children[0].children[0].children[0].updateWorldMatrix(true, true)
-                clicked.current.children[0].children[0].children[0].localToWorld(p.set(0, GOLDENRATIO / 7, 0.5))
-                clicked.current.children[0].children[0].children[0].getWorldQuaternion(q)
-                setActiveItem(clicked.current.linkText)
-                setActiveURL(clicked.current.url)
+                if (clicked.current.children.length !== 0) {
+                    console.log(clicked.current)
+                    clicked.current.children[0].updateWorldMatrix(true, true)
+                    clicked.current.children[0].localToWorld(p.set(0, GOLDENRATIO / 7, 0.5))
+                    clicked.current.children[0].getWorldQuaternion(q)
+
+                    if (clicked.current.friendlyName === "BigMonitor") {
+                        setBigMonTex(githubTex)
+                        setSmallMonTex(githubTex)
+                    }
+                    if (clicked.current.friendlyName === "SmallMonitor") {
+                        // This should be the resume
+                        setBigMonTex(itchTex)
+                        setSmallMonTex(itchTex)
+                    }
+                    if (clicked.current.friendlyName === "Keyboard") {
+                        setSmallMonTex(itchTex)
+                        setBigMonTex(itchTex)
+                    }
+                    if (clicked.current.friendlyName === "Tablet") {
+                        setSmallMonTex(artstationTex)
+                        setBigMonTex(artstationTex)
+                    }
+
+
+                    setActiveItem(clicked.current.parent.parent.linkText)
+                    setActiveURL(clicked.current.parent.parent.url)
+                    setShowPanelL(true)
+                    setShowPanelR(true)
+                }
+
             } else {
                 p.set(0, 1.5, 2)
                 q.identity()
                 setActiveItem()
                 setActiveURL()
+                setShowPanelR(false)
+                setShowPanelL(false)
             }
         })
         useFrame((state, dt) => {
@@ -74,7 +118,8 @@ export const DeskScene = ({ props }) => {
 
     function Interactive({ url, modelName, ...props }) {
         const [hovered, hover] = useState(false)
-        const name = modelName
+        const name = getUuidByString(modelName)
+        const friendlyName = modelName
         useCursor(hovered)
         useFrame((state) => {
         })
@@ -82,6 +127,7 @@ export const DeskScene = ({ props }) => {
             <group {...props} url={url}>
                 < props.model
                     name={name}
+                    friendlyName={friendlyName}
                     onPointerOver={(e) => (e.stopPropagation(), hover(true))}
                     position={[0, GOLDENRATIO / 2, 1]}
                     onPointerOut={() => hover(false)}
@@ -111,6 +157,22 @@ export const DeskScene = ({ props }) => {
                 : null
             }
 
+            {showPanelR === true ?
+                <BigScreen
+                    material={bigMonTex}
+                    position={[0.215, 1.4, .895]}
+                    scale={[.25, .25, .25]}
+                />
+                : null}
+
+            {showPanelL === true ?
+                <BigScreen
+                    material={smallMonTex}
+                    position={[-0.3, 1.4, 0.96]}
+                    scale={[.25, .25, .25]}
+                />
+                : null}
+
             <Selection>
                 <EffectComposer multisampling={8} autoClear={false}>
                     <Outline blur
@@ -123,15 +185,13 @@ export const DeskScene = ({ props }) => {
             <Interactives />
 
             <Box />
+
             <Desk
                 position={[0, GOLDENRATIO / 2, 1]} />
             <Computer
                 position={[0, GOLDENRATIO / 2, 1]} />
             <Chair
                 position={[0, GOLDENRATIO / 2, 1]} />
-
-
-
         </Canvas>
     )
 }
