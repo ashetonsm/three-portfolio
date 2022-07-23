3954b14da1d4f06d1ae870545eff0b82b79d4f5d
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2022-07-22 20:02:53 -0500

    Moved texture change fx to BigScreen.js

--- src/components/models/BigMonitor.js
+++ src/components/models/BigMonitor.js
@@ -1,7 +1,5 @@
 import React, { useRef } from "react";
 import { useGLTF } from "@react-three/drei";
-import { Select } from '@react-three/postprocessing'
-
 
 export default function Model({ ...props }) {
 
@@ -9,28 +7,17 @@ export default function Model({ ...props }) {
     const { nodes, materials } = useGLTF("/three-portfolio/models/BigMonitor.gltf");
 
     return (
-        <Select>
-            <group
-                ref={group}
-                {...props}
-                dispose={null}
-            >
-                <mesh
-                    castShadow
-                    receiveShadow
-                    geometry={nodes.Beeg_Monitor_Screen.geometry}
-                    material={materials["Big Monitor"]}
-                    position={[0.21, 0.5, -0.12]}
-                />
-
-            {/* <mesh
-                position={[0.22, .6, -.1]}
-                scale={[.5, .3, .5]}>
-                <planeGeometry />
-                <meshStandardMaterial />
-            </mesh> */}
-            </group>
-        </Select>
+        <group
+            ref={group}
+            {...props}
+            dispose={null}
+        >
+            <mesh
+                geometry={nodes.Beeg_Monitor_Screen.geometry}
+                material={materials["Big Monitor"]}
+                position={[0.21, 0.5, -0.12]}
+            />
+        </group>
     )
 }
 
