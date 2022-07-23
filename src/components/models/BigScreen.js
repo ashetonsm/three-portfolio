0cbd31571dc67929f794950ba1c56a0d790c7967
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2022-07-22 20:03:13 -0500

    Now handles texture display rather than the items

--- src/components/models/BigScreen.js
+++ src/components/models/BigScreen.js
@@ -1,21 +1,59 @@
-import { useRef, useState } from "react"
-import { useLoader, useFrame } from "@react-three/fiber"
+import React, { useRef, useState } from "react";
+import { useLoader } from "@react-three/fiber";
+import { DoubleSide } from "three";
 import { TextureLoader } from "three/src/loaders/TextureLoader"
-import { useTexture } from "@react-three/drei/core"
-import { DoubleSide } from "three"
 
 export const BigScreen = (props) => {
     const ref = useRef()
-    let currentTexture = props.material
+
+    const [transparentTex, artstationTex, githubTex, itchTex] = useLoader(TextureLoader,
+        [
+            '/three-portfolio/textures/transparentPixel.png',
+            '/three-portfolio/textures/artstation-logo.png',
+            '/three-portfolio/textures/github-logo.png',
+            '/three-portfolio/textures/itchio-logo.png',
+        ])
+
+    const [currentTexture, setCurrentTexture] = useState(transparentTex)
+
+    const handleTexture = (texture) => {
+        switch (texture) {
+            case 0: {
+                setCurrentTexture(transparentTex)
+                break
+            }
+            case 1: {
+                setCurrentTexture(artstationTex)
+                console.log("set to #" + texture)
+                break
+            }
+            case 2: {
+                setCurrentTexture(githubTex)
+                console.log("set to #" + texture)
+                break
+            }
+            case 3: {
+                setCurrentTexture(itchTex)
+                break
+            }
+            default:
+                setCurrentTexture(transparentTex)
+        }
+    }
+
     return (
         <mesh
             {...props}
             ref={ref}
+            position={[0.22, 1.4, 0.9]}
+            scale={[0.25, 0.25, 0.25]}
+            handleTexture={handleTexture}
         >
             <planeGeometry args={[1, 1, 1]} />
             <meshStandardMaterial
                 map={currentTexture}
                 transparent={true}
+                side={DoubleSide}
             />
         </mesh>
     )
