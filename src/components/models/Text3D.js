958b366f19860e772cab98932a37d0815332f3a4
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2022-07-24 14:25:34 -0500

    Slide in works!

--- src/components/models/Text3D.js
+++ src/components/models/Text3D.js
@@ -1,41 +1,58 @@
 import React, { useRef } from 'react'
-import { Html, Text } from '@react-three/drei'
+import { Html } from '@react-three/drei'
+import { useSpring, animated } from '@react-spring/three'
 
 export const Text3D = ({ children, props }) => {
   const ref = useRef()
 
-  return (
-    <Text
-      ref={ref}
-      {...props}
-      position={[]}
-    >
-      <Html {...props}
-        color='#000'
-      >
-        <div
-          style={{
-            backgroundColor: '#ffffff80',
-            width: '20vw',
-            height: '100vh',
-            overflowWrap: 'anywhere',
-            textAlign: 'center',
-            padding: 10,
+  function RenderText() {
+    const { position } = useSpring({
+      to: {
+        position: [0, 0, 0]
+      },
+      from: { position: [-5, 0, 0] },
+      config: { mass: 2, tension: 500, friction: 100 }
+    })
 
-          }}>
-          <h1>
-            {children[0]}
-          </h1>
-          <p>
-            <a
-              href={children[1]}
-              target="_blank"
-              rel="noreferrer noopener"
-            >{children[1]}
-            </a>
-          </p>
-        </div>
-      </Html>
-    </Text>
+    return (
+      <animated.mesh
+        ref={ref}
+        position={position}>
+
+        <Html {...props}
+          color='#000'>
+          <div
+            style={{
+              backgroundColor: '#ffffff80',
+              position: 'absolute',
+              left: '-50vw',
+              top: '-30vh',
+              width: '30vw',
+              height: '100vh',
+              overflowWrap: 'anywhere',
+              textAlign: 'center',
+              padding: 5,
+
+            }}>
+            <h1>
+              {children[0]}
+            </h1>
+            <p>
+              <a
+                href={children[1]}
+                target="_blank"
+                rel="noreferrer noopener"
+              >
+                {children[1]}
+              </a>
+            </p>
+          </div>
+        </Html>
+      </animated.mesh>
+    )
+  }
+
+  return (
+    <RenderText />
   )
 }
