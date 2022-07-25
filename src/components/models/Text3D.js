7fb6eec0af9c68b02f19b7e64f166c3f06479751
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2022-07-25 12:25:44 -0500

    Drawer slides back in on mouse miss

--- src/components/models/Text3D.js
+++ src/components/models/Text3D.js
@@ -1,23 +1,39 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import { Html } from '@react-three/drei'
 import { useSpring, animated } from '@react-spring/three'
 
 export const Text3D = ({ children, props }) => {
   const ref = useRef()
+  const [showDrawer, setShowDrawer] = useState(false)
+
+  const toggleDrawer = (newState) => {
+    setShowDrawer(newState)
+  }
 
   function RenderText() {
     const { position } = useSpring({
       to: {
-        position: [0, 0, 0]
+        position:
+          showDrawer ? [0, 0, 0] :
+            !showDrawer && children[0] === undefined ?
+              [-5, 0, 0] : [-5, 0, 0]
       },
-      from: { position: [-5, 0, 0] },
-      config: { mass: 2, tension: 500, friction: 100 }
+      from: {
+        position:
+          showDrawer ? [-5, 0, 0] :
+            !showDrawer && children[0] === undefined ?
+              [-5, 0, 0] : [0, 0, 0]
+      },
+      config: showDrawer ? { mass: 2, tension: 500, friction: 100 } :
+        { mass: 2, tension: 200, friction: 500 }
     })
 
     return (
       <animated.mesh
         ref={ref}
-        position={position}>
+        position={position}
+        toggleDrawer={toggleDrawer}
+        name="Text3D" >
 
         <Html {...props}
           color='#000'>
