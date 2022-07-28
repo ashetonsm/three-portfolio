45c98b45abbeea313e8a6ea8b61d4fbf5d8e49ca
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2022-07-28 18:48:24 -0500

    Added a new component for new model

---
+++ src/components/models/Mouse.js
@@ -0,0 +1,25 @@
+import React, { useRef } from "react";
+import { useGLTF } from "@react-three/drei";
+
+export default function Model({ ...props }) {
+    const group = useRef();
+    const { nodes, materials } = useGLTF("/three-portfolio/models/Mouse.gltf");
+
+    return (
+        <group
+            ref={group}
+            {...props}
+            dispose={null}>
+
+            <mesh
+                castShadow
+                receiveShadow
+                geometry={nodes.Mouse.geometry}
+                material={materials.Accessories}
+                position={[0.31, 0.39, 0.12]}
+            />
+        </group>
+    )
+}
+
+useGLTF.preload("/Mouse.gltf");
