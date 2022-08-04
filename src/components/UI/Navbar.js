6cf6ec894179e782b31e97f1ae42616e0e3525e8
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2022-08-04 16:22:57 -0500

    Repurposed for link-based navigation

--- src/components/UI/Navbar.js
+++ src/components/UI/Navbar.js
@@ -1,13 +1,34 @@
-export const Navbar = ({children}) => {
-    return (
-        <div>
-            <div>
-            I am the navbar
-            </div>
+import React, { useRef, useState } from 'react'
+import { Html } from '@react-three/drei'
+import { useSpring, animated } from '@react-spring/three'
 
-            {/* This is where the content goes */}
-            {children}
-            
-            </div>
+export const NavBar = ({ children, props }) => {
+
+  const ref = useRef()
+
+    return (
+      <animated.mesh
+        ref={ref}
+        position={[0, 0, 0]}
+        name="NavBar" >
+
+        <Html {...props}
+          color='#000'>
+          <div
+            style={{
+              backgroundColor: '#00000080',
+              position: 'absolute',
+              left: '30vw',
+              top: '-50vh',
+              width: '20vw',
+              height: '100vh',
+              overflowWrap: 'anywhere',
+              textAlign: 'center',
+              paddingTop: '30vh',
+            }}>
+                {children}
+          </div>
+        </Html>
+      </animated.mesh>
     )
-}
+  }
