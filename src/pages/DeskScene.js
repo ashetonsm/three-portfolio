ef0295719cd4f4097558249bdea80296be0541bd
Asheton S. M <98793112+ashetonsm@users.noreply.github.com>
2025-01-02 20:09:02 -0600

    Updated with example from CodeSandbox
    https://codesandbox.io/p/sandbox/ambient-light-m56qo

--- src/pages/DeskScene.js
+++ src/pages/DeskScene.js
@@ -1,201 +1,63 @@
-import * as THREE from 'three'
-import { Canvas, useFrame } from "@react-three/fiber"
-import { useRef, useState, useEffect, Suspense } from "react"
-import { useCursor, Html, useProgress } from '@react-three/drei'
-import BigMonitor from '../components/models/BigMonitor'
-import SmallMonitor from '../components/models/SmallMonitor'
-import Keyboard from '../components/models/Keyboard'
-import Tablet from '../components/models/Tablet'
-import Desk from '../components/models/Desk'
-import Computer from '../components/models/Computer'
-import Mouse from '../components/models/Mouse'
-import { ScreenOverlay } from '../components/models/ScreenOverlay'
-import { Box } from '../components/models/Box'
-import { TextDrawer } from '../components/UI/TextDrawer'
-import { NavBar } from '../components/UI/Navbar'
+import React, { useState, Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import { Physics, usePlane, useBox } from "@react-three/cannon";
 
-export const DeskScene = () => {
+function Plane() {
+  const [ref] = usePlane(() => ({
+    rotation: [Math.PI * -0.5, 0, 0],
+    position: [0, -2, 0]
+  }));
+  return (
+    <mesh receiveShadow ref={ref}>
+      <planeGeometry args={[10, 10]} />
+      <meshStandardMaterial color={0x00ff00} />
+    </mesh>
+  );
+}
 
-    const camX = 0
-    const camY = 1
-    const camZ = 0.90
+function Box(props) {
+  const [ref] = useBox(() => ({ mass: 1, ...props }));
+  return (
+    <mesh receiveShadow castShadow ref={ref}>
+      <boxGeometry />
+      <meshStandardMaterial color={props.color} />
+    </mesh>
+  );
+}
 
-    const [activeItem, setActiveItem] = useState("Welcome!")
-    const [activeURL, setActiveURL] = useState("Please select a model or a link.")
-    const [currentItem, setCurrentItem] = useState(null)
+export default function App() {
 
-    const interactives = [
-        { model: BigMonitor, modelName: "BigMonitor", linkText: "GitHub", url: "https://github.com/ashetonsm" },
-        { model: SmallMonitor, modelName: "SmallMonitor", linkText: "Resumé", url: "https://www.dropbox.com/s/gplszprw31msqja/Mayfield_A_Resume_R.pdf?dl=0" },
-        { model: Keyboard, modelName: "Keyboard", linkText: "Itch.io", url: "https://nnneato.itch.io/" },
-        { model: Tablet, modelName: "Tablet", linkText: "ArtStation", url: "https://artstation.com/ashetonsm" },
-    ]
+  return (
+    <div className="App">
+      <Canvas shadows style={{ height: '100vh' }} >
+        <Physics>
+          <Plane />
+          <Box
+            position={[0, 2, 0]}
+            rotation={[Math.PI * 0.25, Math.PI * 0.25, 0]}
+            color={0xff0000}
+          />
+          <Box
+            position={[0.5, 4, 0]}
+            rotation={[Math.PI * 0.25, Math.PI * 0.25, 0]}
+            color={0x0000ff}
+          />
+        </Physics>
 
-    const setActive = (newItem, newURL) => {
-        setActiveItem(newItem)
-        setActiveURL(newURL)
-    }
-
-    const setCurrent = (modelName, linkText, linkUrl) => {
-        setCurrentItem(modelName)
-        setActive(linkText, linkUrl)
-    }
-
-    function Interactives({ p = new THREE.Vector3(camX, camY, camZ) }) {
-
-        const ref = useRef()
-        const screens = useRef()
-        let drawer = useRef()
-        const linksRef = useRef()
-
-        useEffect(() => {
-
-            drawer.current = ref.current.parent.getObjectByName("TextDrawer")
-            screens.current = ref.current.parent.getObjectByName("Screens")
-
-            // drawer.current.toggleDrawer(true)
-
-            switch (currentItem) {
-                case "SmallMonitor":
-                    screens.current.handleTexture(0)
-                    p.setX(-0.4)
-                    break
-                case "Tablet":
-                    screens.current.handleTexture(1)
-                    p.setX(0.4)
-                    break
-                case "BigMonitor":
-                    screens.current.handleTexture(2)
-                    p.setX(0.2)
-                    break
-                case "Keyboard":
-                    screens.current.handleTexture(3)
-                    p.setX(-0.2)
-                    break
-                default:
-                    screens.current.handleTexture()
-                    // drawer.current.toggleDrawer(false)
-                    p.setX(0)
-            }
-        })
-
-        useFrame((state) => {
-
-            if (currentItem) {
-                state.camera.position.lerp(p, 0.025)
-                // TextDrawer
-                state.scene.children[4].position.set(state.camera.position.x, state.camera.position.y + 0.4, 0)
-
-                // NavLinks
-                state.scene.children[3].position.set(state.camera.position.x, state.camera.position.y, 0)
-            } else {
-                // TextDrawer
-                state.scene.children[4].position.set(state.camera.position.x, state.camera.position.y + 0.4, 0)
-
-                // NavLinks
-                state.scene.children[3].position.set(state.camera.position.x, state.camera.position.y, 0)
-            }
-        }, 0)
-
-        return (
-            <>
-                {/* This is the object that our camera gets its coordinates from */}
-                <group
-                    ref={ref}
-                    onClick={(e) => setCurrent(e.object.name, e.object.linkText, e.object.url)}
-                    name="Interactive Meshes"
-                >
-                    {interactives.map((props) =>
-                        <Interactive
-                            key={props.modelName}
-                            name={props.modelName}
-                            model={props.model}
-                            linkText={props.linkText}
-                            url={props.url}
-                            {...props} />
-                    )}
-                </group>
-
-                {/* This is how the NavBar links are rendered. They need the info from the models to zoom in. */}
-                <group
-                    ref={linksRef}
-                    name="HTML Links" >
-
-                    <Html>
-                        <div style={{
-                            width: '100vw',
-                            padding: 'none',
-                            textAlign: 'center',
-                            left: '-50vw',
-                            position: 'absolute',
-                        }}>
-
-                            {interactives.map((props) =>
-                                <NavBar
-                                    key={props.modelName}
-                                    name={props.modelName}
-                                    model={props.model}
-                                    linkText={props.linkText}
-                                    url={props.url}
-                                    sendMessage={(modelName, linkText, linkUrl) => setCurrent(modelName, linkText, linkUrl)}
-                                    {...props} />
-                            )}
-                        </div>
-                    </Html>
-                </group>
-
-                <TextDrawer>
-                    {activeItem}
-                    {activeURL}
-                </TextDrawer>
-
-                <ScreenOverlay name="Screens" />
-
-            </>
-        )
-    }
-
-    function Interactive({ url, modelName, linkText, ...props }) {
-        const [hovered, hover] = useState(false)
-        useCursor(hovered)
-        return (
-            < props.model
-                name={modelName}
-                linkText={linkText}
-                url={url}
-                position={[0, 0, 0]}
-                onPointerOver={(e) => (hover(true))}
-                onPointerOut={() => hover(false)}
-            />
-        )
-    }
-
-    function Loader() {
-        const { progress } = useProgress()
-        return <Html center>{Math.round(progress)} % loaded</Html>
-    }
-
-    return (
-        <Canvas shadows style={{ height: '100vh' }} camera={{
-            position: [camX, camY, camZ],
-            rotation: [-0.4, 0, 0],
-        }}>
-            <Suspense fallback={<Loader />}>
-                <ambientLight intensity={0.5} />
-                <spotLight
-                    castShadow
-                    shadow-bias={-0.00002}
-                    position={[0, 40, 10]}
-                    angle={Math.PI / 50}
-                    intensity={1}
-                    color={'#effeff'} />
-
-                <Interactives />
-                <Box position={[0, 0.05, 1]} />
-                <Desk />
-                <Computer />
-                <Mouse />
-            </Suspense>
-        </Canvas>
-    )
+        <ambientLight color={0xffffff} intensity={0.1} />
+        <pointLight
+          color={0xffffff}
+          castShadow
+          shadowMapHeight={1024}
+          shadowMapWidth={1024}
+          position={[0, 3, 2]}
+          intensity={0.5}
+        />
+        <OrbitControls />
+        <Suspense fallback={null}>
+        </Suspense>
+      </Canvas>
+    </div>
+  );
 }
